6af94cda92ee0c95e47175c90da496ac8fd63e45
letmezep <letmezep@gmail.com>
2026-06-28 00:06:47 +0800

    feat: implement burger menu (Sun, Jun 28, 2026 12:06:47 AM)

--- shelter/index.js
+++ shelter/index.js
@@ -5,31 +5,43 @@ const nextSlide = document.querySelector(".next-slide");
 let SlideIndex = 0;
 const SliderLength = document.querySelectorAll(".pet-card").length;
 
-
-
-function turnSlide () {
-    console.log("SliderLength: ", SliderLength);
-    sliderLine.style.transform = `translateX(-${SlideIndex * 370}px)`;
+function turnSlide() {
+  console.log("SliderLength: ", SliderLength);
+  sliderLine.style.transform = `translateX(-${SlideIndex * 370}px)`;
 }
 
 prevSlide.addEventListener("click", () => {
-    SlideIndex--;
-    if (SlideIndex >= 0) {
-        turnSlide();
-    } else {
-        SlideIndex= SliderLength - 3;
-        turnSlide();
-    }
-    
-})
+  SlideIndex--;
+  if (SlideIndex >= 0) {
+    turnSlide();
+  } else {
+    SlideIndex = SliderLength - 3;
+    turnSlide();
+  }
+});
 
 nextSlide.addEventListener("click", () => {
-    SlideIndex++;
-    if (SlideIndex < SliderLength - 3) {
-        turnSlide();
-    } else {
-        SlideIndex= 0;
-        turnSlide();
-    }
-    
-})
+  SlideIndex++;
+  if (SlideIndex < SliderLength - 3) {
+    turnSlide();
+  } else {
+    SlideIndex = 0;
+    turnSlide();
+  }
+});
+
+const burger = document.querySelector(".burger-btn");
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelectorAll(".menu a");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  burger.classList.toggle("close");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("open");
+    burger.classList.toggle("close");
+  });
+});
